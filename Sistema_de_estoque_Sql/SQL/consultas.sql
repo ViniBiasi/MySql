65072aca1928814a10edc6ec0e96f5c592a14c66
vinicius Biasi <viniciusbiasi301@gmail.com>
2026-06-17 16:16:42 -0300

    Subindo os dados

--- Sistema_de_estoque_Sql/SQL/consultas.sql
+++ Sistema_de_estoque_Sql/SQL/consultas.sql
@@ -1,2 +1,22 @@
-SHOW TABLES;
+SELECT p.nome, c.nome_categoria
+FROM produtos p
+JOIN categorias c
+ON p.id_categoria = c.id_categoria;
 
+SELECT p.nome, f.nome_fornecedor
+FROM produtos p
+JOIN fornecedores f
+ON p.id_fornecedor = f.id_fornecedor;
+
+SELECT c.nome_categoria, COUNT(*) AS quantidade
+FROM produtos p
+JOIN categorias c
+ON p.id_categoria = c.id_categoria
+GROUP BY c.nome_categoria;
+
+SELECT nome, quantidade_estoque
+FROM produtos
+WHERE quantidade_estoque < 20;
+
+SELECT SUM(preco * quantidade_estoque) AS valor_total
+FROM produtos;
